2d49cbacaba6482cdc2b8d32b1576fff5edef3e8
folarin-codes <suberufolarin@gmail.com>
2022-09-13 01:05:08 +0100

    project initialisation

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+
+class App extends Component{
+  render() {
+    return (
+      <>
+        <h1>Welcome to react</h1>
+      </>
+    )
+  }
+}
+
+export default App
